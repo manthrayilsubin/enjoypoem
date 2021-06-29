9c78fa732872ea6bcfc7bf859920ae62b7c8a038
Subin Babu <manthrayilsubin@gmail.com>
2021-06-29 08:18:57 +0000

    initial commit

---
+++ intro.php
@@ -0,0 +1,85 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+ <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<style>
+/* Google Fonts */
+@import url(https://fonts.googleapis.com/css?family=Anonymous+Pro);
+
+/* Global */
+html{
+  min-height: 100%;
+  overflow: hidden;
+}
+body{
+  height: calc(100vh - 8em);
+  padding: 4em;
+  color: rgba(255,255,255,.75);
+  font-family: 'Anonymous Pro', monospace;  
+  background-color: rgb(25,25,25);  
+}
+.line-1{
+    position: relative;
+    top: 50%;  
+    width: 30em;
+    margin: 0 auto;
+    border-right: 2px solid rgba(255,255,255,.75);
+    font-size: 180%;
+    text-align: center;
+    white-space: nowrap;
+    overflow: hidden;
+    transform: translateY(-50%);    
+}
+@media screen and (max-width: 600px) {
+  #typemsg {
+    visibility: hidden;
+    display: none;
+  }
+}
+/* Animation */
+.anim-typewriter{
+  animation: typewriter 6s steps(44) 1s 1 normal both,
+             blinkTextCursor 600ms steps(44) infinite normal;
+}
+@keyframes typewriter{
+  from{width: 0;}
+  to{width: 25em;}
+}
+@keyframes blinkTextCursor{
+  from{border-right-color: rgba(255,255,255,.75);}
+  to{border-right-color: transparent;}
+}
+
+
+
+</style>
+ <title>Enjoying a Poem is Our Birth Right.</title>
+</head>
+<body>
+ <div class="container">
+  <div class="row">
+  <img src="https://play-lh.googleusercontent.com/tJIjI-Gp8BnopJYWYpbYTqqnjku46bcx5eN1iV61VpfsF3cM2pAYqC9PF4um5f1d15U" style="width: 17%;"</img>
+  </div>
+  <br><br>
+  <div class="row">
+  <p>Enjoying a Poem is our birth right!</p>
+  </div>
+  <br><br>
+  <div class="row">
+<p class="line-1 anim-typewriter" id="typemsg">വരുംകാല തലമുറയ്ക്ക് ഒരു കൈത്തിരി തെളിക്കാം.</p>
+<p class="line-1 anim-typewriter" id="typemsg">വാക്കുകൾ  ഹൃദയങ്ങളിൽ തറയ്ക്കട്ടെ .</p>
+
+</div>
+<br><br>
+  <div class="row">
+<p >Click To Enter</p>
+</div>
+</div>
+
+</body>
+</html>
